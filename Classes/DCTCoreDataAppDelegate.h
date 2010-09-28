68fcfcbdf7598d2021904f1fe94a8cb72f074e6f
Daniel Tull <dt@danieltull.co.uk>
2010-09-28 09:52:52 +0100

    Added example showing how to use the automated setup.

--- Classes/DCTCoreDataAppDelegate.h
+++ Classes/DCTCoreDataAppDelegate.h
@@ -10,17 +10,10 @@
 
 @interface DCTCoreDataAppDelegate : NSObject <UIApplicationDelegate> {
 	UIWindow *window;
-	NSManagedObjectContext *managedObjectContext;
-	NSManagedObjectModel *managedObjectModel;
-	NSPersistentStoreCoordinator *persistentStoreCoordinator;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (NSString *)applicationDocumentsDirectory;
+- (NSManagedObjectContext *)managedObjectContext;
 
 @end
