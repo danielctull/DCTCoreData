07e67a0e61bf8bfff0011d86e32e825e187187f5
danielctull <dt@danieltull.co.uk>
2010-12-04 22:47:01 +0000

    Add asynchronous fetching.

--- DCTCoreData/NSManagedObjectContext+DCTAsynchronousTasks.h
+++ DCTCoreData/NSManagedObjectContext+DCTAsynchronousTasks.h
@@ -9,12 +9,19 @@
 #import <Foundation/Foundation.h>
 
 typedef void (^DCTManagedObjectContextBlock) (NSManagedObjectContext *managedObjectContext);
+typedef void (^DCTFetchRequestCallbackBlock) (NSArray *fetchedObjects, NSError *error);
 
 @interface NSManagedObjectContext (DCTAsynchronousTasks)
 
 - (void)dct_asynchronousOperationWithBlock:(DCTManagedObjectContextBlock)block;
-
 - (void)dct_asynchronousOperationWithCallbackQueue:(dispatch_queue_t)queue
 											 block:(DCTManagedObjectContextBlock)block;
+
+- (void)dct_asynchronousFetch:(NSFetchRequest *)fetchRequest
+			WithCallbackBlock:(DCTFetchRequestCallbackBlock)callbackBlock;
+
+- (void)dct_asynchronousFetchRequest:(NSFetchRequest *)fetchRequest
+				   withCallbackQueue:(dispatch_queue_t)callbackQueue
+							   block:(DCTFetchRequestCallbackBlock)callbackBlock;
 
 @end
--- DCTCoreData/NSManagedObjectContext+DCTAsynchronousTasks.m
+++ DCTCoreData/NSManagedObjectContext+DCTAsynchronousTasks.m
@@ -11,16 +11,17 @@
 
 @interface NSManagedObjectContext ()
 - (void)dctInternal_threadedContextDidSave:(NSNotification *)notification;
+- (BOOL)dctInternal_raiseExceptionIfNotMainThread;
 @end
 
 @implementation NSManagedObjectContext (DCTAsynchronousTasks)
 
+#pragma mark -
+#pragma mark Modification methods
+
 - (void)dct_asynchronousOperationWithBlock:(DCTManagedObjectContextBlock)block {
 	
-	if (![[NSThread currentThread] isEqual:[NSThread mainThread]])
-		[[NSException exceptionWithName:@"DCTManagedObjectContextBackgroundingNotMainThreadException"
-								 reason:@"Calling to perform a background operation on something which isn't the main thread." 
-							   userInfo:nil] raise];
+	if ([self dctInternal_raiseExceptionIfNotMainThread]) return;
 	
 	[self dct_asynchronousOperationWithCallbackQueue:dispatch_get_main_queue() block:block];
 }
@@ -55,6 +56,67 @@
 			[threadedContext release];
 		});		
 	});
+}
+
+#pragma mark -
+#pragma mark Fetch methods
+
+- (void)dct_asynchronousFetch:(NSFetchRequest *)fetchRequest
+			WithCallbackBlock:(DCTFetchRequestCallbackBlock)callbackBlock {
+	
+	if ([self dctInternal_raiseExceptionIfNotMainThread]) return;
+	
+	[self dct_asynchronousFetchRequest:fetchRequest 
+					 withCallbackQueue:dispatch_get_main_queue()
+								 block:callbackBlock];
+}
+
+- (void)dct_asynchronousFetchRequest:(NSFetchRequest *)fetchRequest
+				   withCallbackQueue:(dispatch_queue_t)callbackQueue
+							   block:(DCTFetchRequestCallbackBlock)callbackBlock {
+	
+	NSManagedObjectContext *threadedContext = [[NSManagedObjectContext alloc] init];
+	[threadedContext setPersistentStoreCoordinator:[self persistentStoreCoordinator]];
+	
+	dispatch_queue_t asyncQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+	
+	dispatch_async(asyncQueue, ^{
+		
+		NSError *error = nil;
+		NSArray *array = [threadedContext executeFetchRequest:fetchRequest error:&error];
+		
+		NSMutableArray *objectIDs = [NSMutableArray arrayWithCapacity:[array count]];
+		
+		for (NSManagedObject *mo in array)
+			[objectIDs addObject:[mo objectID]];
+		
+		
+		dispatch_async(callbackQueue, ^{
+			
+			NSMutableArray *returnedObjects = [NSMutableArray arrayWithCapacity:[objectIDs count]];
+			
+			for (NSManagedObjectID *objectID in array)		
+				[returnedObjects addObject:[self objectWithID:objectID]];
+			
+			callbackBlock([NSArray arrayWithArray:returnedObjects], error);
+		});
+			
+	});	
+	
+}
+
+#pragma mark -
+#pragma mark Internal methods
+
+- (BOOL)dctInternal_raiseExceptionIfNotMainThread {
+	if (![[NSThread currentThread] isEqual:[NSThread mainThread]]) {
+		[[NSException exceptionWithName:@"DCTManagedObjectContextBackgroundingNotMainThreadException"
+								 reason:@"Calling to perform a background operation on something which isn't the main thread." 
+							   userInfo:nil] raise];
+		return YES;
+	}
+	
+	return NO;	
 }
 
 - (void)dctInternal_threadedContextDidSave:(NSNotification *)notification {
